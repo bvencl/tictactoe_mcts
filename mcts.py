16c2f2fe361be2e29c062b34e14c4c1ea6734f8c
Bodi.Vencel <150474053+bvencl@users.noreply.github.com>
2024-08-27 15:20:56 +0200

    twoplayer mukodik

--- mcts.py
+++ mcts.py
@@ -1,9 +1,127 @@
 from tictactoe import Board
-from node import Node, side
 import constants
 import numpy as np
 from typing import Optional, List
 import copy, random, sys
+
+side = 0
+
+
+class Node:
+    def __init__(
+        self, parent_node: Optional["Node"], board: Board, action: Optional[int], player
+    ) -> None:
+
+        self.parent = parent_node
+        self.action = action
+        self.player = player
+        self.visit_count = [0, 0]
+        self.score = [0.0, 0.0]
+        self.ucb1 = [np.inf, np.inf]
+        self.state = copy.deepcopy(board)
+        if self.action is not None:
+            self.state.step(self.action, self.player)
+        self.children = []
+        self.legal_actions = self.state.free_squares()
+        self.is_terminal_node = self.state.is_terminal()
+        if self.player == 1:
+            self.side = 0
+        else:
+            self.side = 1
+
+    def evaluate(self) -> Optional[List[float]]:
+        terminal_state = self.state.is_terminal()
+        if terminal_state == 1:
+            return [constants.WIN_REWARD, constants.LOSS_REWARD]
+        if terminal_state == -1:
+            return [constants.LOSS_REWARD, constants.WIN_REWARD]
+        if terminal_state == 0:
+            return [constants.DRAW_REWARD, constants.DRAW_REWARD]
+        else:
+            return None
+
+    def is_leaf(self):
+        if self.children == [] or self.is_terminal_node is not None:
+            return True
+        return False
+
+    def expand_node(self):
+        if self.is_terminal_node is not None:
+            return self.evaluate()
+        else:
+            self.children = [
+                Node(
+                    parent_node=self,
+                    board=self.state,
+                    action=action,
+                    player=-self.player,
+                )
+                for action in self.legal_actions
+            ]
+            self.children_count = len(self.children)
+
+    def best_child(self) -> "Node":
+        if self.is_terminal_node is not None:
+            return self
+        for child in self.children:
+            if child is not None:
+                child.calculate_ucb1()
+        best = self.children[0]
+        for child in self.children:
+            if child is not None and child.ucb1[side % 2] > best.ucb1[side % 2]:
+                best = child
+        return best
+
+    def robust_child(self) -> "Node":
+        if not self.children:
+            raise ValueError("No children to choose from in most_robust_child.")
+        best = max(
+            self.children,
+            key=lambda child: child.visit_count[side % 2],
+        )
+        return best
+
+    def confident_child(self) -> "Node":
+        if not self.children:
+            raise ValueError("No children to choose from in confident_child.")
+        best = max(
+            (child for child in self.children if child.ucb1[side % 2] != np.inf),
+            key=lambda child: child.ucb1[side % 2],
+        )
+        return best
+
+    def calculate_ucb1(self) -> List[float]:
+        global side
+        if self.visit_count[side % 2] == 0:
+            self.ucb1[side % 2] = np.inf
+            return self.ucb1
+        else:
+            if self.parent is None:
+                exploration_side = 0
+            else:
+                exploration_side = (
+                    2
+                    * constants.UCB_EXPLORATION_CONSTANT
+                    * np.sqrt(
+                        (2 * np.log(self.parent.visit_count[side % 2]))
+                        / self.visit_count[side % 2]
+                    )
+                )
+
+            exploitation_side = self.score[side % 2] / self.visit_count[side % 2]
+
+            ucb1_player_side = exploitation_side + exploration_side
+
+            self.ucb1[side % 2] = ucb1_player_side
+            return self.ucb1
+
+    def find_node_with_player_action(self, player_action) -> "Node":
+        for child in self.children:
+            if child.action == player_action:
+                return child
+        print("mégnagyobb baj van")
+        return child
+
 
 class Agent:
 
@@ -21,7 +139,11 @@ class Agent:
         while self.current_global_state.is_terminal() is None:
             if side % 2 == 0 and twoplayermode is True:
                 player_step = self.player_turn()
-                print("-----------------------------------------------fasz")
+                print(f"Players step: {player_step}")
+                self.update_global_state(player_step)
+                self.current_global_node = (
+                    self.current_global_node.find_node_with_player_action(player_step)
+                )
             else:
                 for _ in range(depth):
                     print(_)
@@ -44,10 +166,10 @@ class Agent:
                             self.backpropagation(evaluation)
                     self.current_node = self.current_global_node
 
-            self.current_global_node = self.current_global_node.robust_child()
-            self.update_global_state()
-            self.current_global_state.render()
+                self.current_global_node = self.current_global_node.robust_child()
+                self.update_global_state(None)
             side += 1
+            self.current_global_state.render()
 
     def backpropagation(self, result):
         global side
@@ -67,13 +189,18 @@ class Agent:
             current_player = -current_player
         return node.evaluate()
 
-    def update_global_state(self):
-        self.current_global_state.step(
-            self.current_global_node.action, self.current_global_node.player
-        )
+    def update_global_state(self, player_step: Optional[int]):
+        if player_step is None:
+            self.current_global_state.step(
+                self.current_global_node.action, self.current_global_node.player
+            )
+        else:
+            self.current_global_state.step(
+                player_step, -self.current_global_node.player
+            )
 
     def player_turn(self) -> int:
-        player_step = input("Te jössz bohóc")
+        player_step = input("Te jössz bohóc:\n")
         return int(player_step)
 
 
